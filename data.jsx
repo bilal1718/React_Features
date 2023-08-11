b039b5a43e12a8440a564af4c359e4ef8ed9fb2c
Muhammad Bilal <muhbil171845@gmail.com>
2023-08-11 11:44:24 +0500

    Add default data
    The array of objects containing five to six enteries

---
+++ data.jsx
@@ -0,0 +1,68 @@
+import img1 from "./Images/img1.png"
+import img2 from "./Images/img2.png"
+import img3 from "./Images/img3.png"
+import img4 from "./Images/img4.png"
+import img5 from "./Images/img5.png"
+import img6 from "./Images/img6.png"
+import img7 from "./Images/img7.png"
+import img8 from "./Images/img8.png"
+
+
+export default [
+    {
+      id:1,
+      img:img1,
+      description:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+      rating:5,
+      price:500
+    },
+    {
+        id:2,
+        img:img2,
+        description:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+        rating:4,
+        price:389  
+    },
+    {
+        id:3,
+        img:img3,
+        description:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+        rating:4,
+        price:276
+    },
+    {
+        id:4,
+        img:img4,
+        description:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+        rating:5,
+        price:700 
+    },
+    {
+        id:5,
+        img:img5,
+        description:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+        rating:3,
+        price:200
+    },
+    {
+        id:6,
+      img:img6,
+      description:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+      rating:4,
+      price:310
+    },
+    {
+        id:7,
+        img:img7,
+        description:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+        rating:2,
+        price:350
+    },
+    {
+        id:8,
+      img:img8,
+      description:"Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport",
+      rating:5,
+      price:654
+    },
+]
